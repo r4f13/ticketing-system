8747cb5f589038dd7ccf1dfc642d48ea6dda01ab
r4f13 <139564555+r4f13@users.noreply.github.com>
2024-07-01 20:04:12 +0700

    db schema finished

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { TicketController } from './ticket/ticket.controller';
+import { TicketModule } from './ticket/ticket.module';
 
 @Module({
   imports: [AuthModule,
     ConfigModule.forRoot({
       isGlobal: true,
-    })],
-  controllers: [AppController],
+    }),
+    TicketModule],
+  controllers: [AppController, TicketController],
   providers: [AppService],
 })
 export class AppModule {}
